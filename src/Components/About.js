3dfc80098ca4b7cac9ed98584017acf0222a6182
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-09-07 15:09:38 +0300

    Detailed About Us

---
+++ src/Components/About.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+function About(){
+
+    return (
+    <div className = "creatorsText">
+        <img className="aboutUsImage"src="https://i.imgur.com/WQf5ON8.png" alt="aboutus"/>
+        <h2 id="header1">WHO CREATED THE FLOWER SHOP (APP)?</h2>
+        <p id= "creators">This Project was created by <strong>Nicholas Kung'u Kamau</strong>using Ruby and React JavaScript. The back-end is solely based on Ruby and the front-end is React JavaScript.</p>
+
+        <h2 id="header2">WHAT IS THE FLOWER SHOP?</h2>
+        <p id="aboutShop">The Flower Shop is the online florist company that makes sending flowers as enjoyable as receiving. It's a seamless, one-of-a-kind experience to help make anyone's day from coast to coast.We source on-trend flowers and create one-of-a-kind arrangements you won't find anywhere else! We work with the best-in-class designers to offer stunning bouquets and curated plants that fit every occasion.</p>
+
+        <h2 id="shopHours">HOURS OF OPERATION</h2>
+        <p id="hours">Our shop is open Monday through Friday 8:30 a.m. to 5:30 p.m, Saturday 9:00 a.m. to 3:00 p.m, and Sunday 9:00 a.m. to 1:00 p.m.</p>
+
+        <h4 id="header3">Holiday Hours & Summer Hours are subject to change.</h4>
+        <p id="holidayHours">Our shop closes for the following holidays: New Year’s Day, Easter, Mother’s Day, Father’s Day, and Christmas Day. Please give us a call so we can fill your order +254-70803-679903!</p>
+        
+        <h2 id="header4">TIMED DELIVERIES</h2>
+        <p id="timedDelivery">We also accept timed deliveries and we will do our best to accommodate specific times but we cannot guarantee it. We will inform you of any issue we may have with your timed delivery.We also accept same day deliveries, but please understand depending on the time of day and the location of the delivery will determine whether we are able to accept.</p>
+
+        <h1 id="header5">Thank you for using our app!</h1>
+        <h2 id="header6">Happy shopping!</h2>
+        
+        <img id="aboutUsLogo"src="https://i.imgur.com/Fxoz84f.png" alt="aboutUsLogo"/>
+    </div>
+
+    )
+}
+
+export default About
