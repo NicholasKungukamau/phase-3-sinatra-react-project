943275db31608fcc612f7b7e65e9bbc904b39886
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-09-07 16:11:36 +0300

    add sample image data

---
+++ src/component/ImageData.js
@@ -0,0 +1,20 @@
+export const ImageData=[
+    {
+        image: 'https://i.imgur.com/f4f9K0g.png'
+    },
+    {
+        image: 'https://i.imgur.com/MsdnJ5r.png'
+    },
+    {
+        image: 'https://i.imgur.com/s7VjsFA.png'
+    },
+    {
+        image: 'https://i.imgur.com/o6yZM9c.png'
+    },
+    {
+        image: 'https://i.imgur.com/1QmpWfF.png'
+    },
+    {
+        image: 'https://i.imgur.com/V9d9maN.png'
+    }
+]
